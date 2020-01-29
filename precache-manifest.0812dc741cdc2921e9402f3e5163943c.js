6ba959a35bdbcd586d62e652c36c734d73e4d772
vorzawk <21965736+vorzawk@users.noreply.github.com>
2020-01-29 17:38:31 +0530

    Updates

---
+++ precache-manifest.0812dc741cdc2921e9402f3e5163943c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02d1ccbb0d99f287ffd780ea50ec76e",
+    "url": "/word-guess-app/index.html"
+  },
+  {
+    "revision": "5e0a8935c98cccaac40d",
+    "url": "/word-guess-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "22cd1552baa88bd09adc",
+    "url": "/word-guess-app/static/js/2.8277c6d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/word-guess-app/static/js/2.8277c6d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e0a8935c98cccaac40d",
+    "url": "/word-guess-app/static/js/main.fbacb804.chunk.js"
+  },
+  {
+    "revision": "8e866beaac906bc8ab0b",
+    "url": "/word-guess-app/static/js/runtime-main.ef906112.js"
+  }
+]);
